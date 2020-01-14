70e35412023f904071026db3208acaf721bc1727
Calvin Hinzer <calvinhinzer@Calvin-sein-MacBook-Pro.local>
2020-01-14 11:23:32 +0100

    Updates

---
+++ precache-manifest.9d7a559f3e063589b5ea7c0776e8a75d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed51110e1b52a3dd04144494cec9d8be",
+    "url": "/wishlist/index.html"
+  },
+  {
+    "revision": "a35e687480b9d652b2ae",
+    "url": "/wishlist/static/css/main.f3631bad.chunk.css"
+  },
+  {
+    "revision": "b07591a052f1876b7e51",
+    "url": "/wishlist/static/js/2.29fdb2be.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/wishlist/static/js/2.29fdb2be.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a35e687480b9d652b2ae",
+    "url": "/wishlist/static/js/main.66d5d4a4.chunk.js"
+  },
+  {
+    "revision": "2af4789034577f2a46c8",
+    "url": "/wishlist/static/js/runtime-main.bb921753.js"
+  },
+  {
+    "revision": "1bbfb923e42afb45fdd0637e8ffde4c2",
+    "url": "/wishlist/static/media/submit-icon.1bbfb923.svg"
+  }
+]);
